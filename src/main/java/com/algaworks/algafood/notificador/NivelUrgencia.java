d3ac7a05eb7bfc4bf0f1bd08b9abad2cab60ef16
Júlia Lima <juliafclima@hotmail.com>
2024-05-10 13:29:09 -0300

    feat: add jpa

--- src/main/java/com/algaworks/algafood/notificador/NivelUrgencia.java
+++
@@ -1,7 +0,0 @@
-package com.algaworks.algafood.notificador;
-
-public enum NivelUrgencia {
-
-	URGENTE,
-	SEM_URGENCIA
-}
--- src/main/java/com/algaworks/algafood/notificador/Notificador.java
+++
@@ -1,8 +0,0 @@
-package com.algaworks.algafood.notificador;
-
-import com.algaworks.algafood.modelo.Cliente;
-
-public interface Notificador {
-
-	void notificar(Cliente cliente, String mensagem);
-}
