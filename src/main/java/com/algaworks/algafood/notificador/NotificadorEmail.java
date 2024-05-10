c4d64716e05a7d6ce06f89b72212d9ffb8b8217c
Júlia Lima <juliafclima@hotmail.com>
2024-05-10 10:05:35 -0300

    feat: spring e injecao de dependencias

--- src/main/java/com/algaworks/algafood/notificador/NotificadorEmail.java
+++ src/main/java/com/algaworks/algafood/notificador/NotificadorEmail.java
@@ -1,21 +1,22 @@
 package com.algaworks.algafood.notificador;
 
-import org.springframework.context.annotation.Profile;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.algaworks.algafood.modelo.Cliente;
 
-@Profile("prod")
 @TipoDoNotificador(NivelUrgencia.SEM_URGENCIA)
 @Component
 public class NotificadorEmail implements Notificador {
 
-	public NotificadorEmail() {
-		System.out.println("NotificadorEmail normal");
-	}
-
+	@Autowired
+	private NotificadorProperties properties;
+	
 	@Override
 	public void notificar(Cliente cliente, String mensagem) {
+		System.out.println("Host: " + properties.getHostServidor());
+		System.out.println("Porta: " + properties.getPortaServidor());
+		
 		System.out.printf("Notificando %s através do e-mail %s: %s\n", 
 				cliente.getNome(), cliente.getEmail(), mensagem);
 	}
