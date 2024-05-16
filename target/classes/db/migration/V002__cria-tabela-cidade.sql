ff487730a466d6deeb79e7c2edfa38e7af0b455a
Júlia Lima <juliafclima@hotmail.com>
2024-05-16 15:17:37 -0300

    feat: criacao de tabelas pelo flyway

---
+++ target/classes/db/migration/V002__cria-tabela-cidade.sql
@@ -0,0 +1,8 @@
+CREATE TABLE cidade (
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    nome_cidade VARCHAR(80) NOT NULL,
+    nome_estado VARCHAR(80) NOT NULL,
+    
+    PRIMARY KEY (id)
+    
+) ENGINE=INNODB DEFAULT CHARSET=UTF8;
