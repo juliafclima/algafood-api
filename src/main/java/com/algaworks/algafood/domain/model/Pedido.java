8805b872b654ba21a8e34d5f2957a433cc40fb62
Júlia Lima <juliafclima@hotmail.com>
2024-05-23 09:26:32 -0300

    aftermigrate update

--- src/main/java/com/algaworks/algafood/domain/model/Pedido.java
+++ src/main/java/com/algaworks/algafood/domain/model/Pedido.java
@@ -7,6 +7,8 @@ import java.util.List;
 
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -30,19 +32,24 @@ public class Pedido {
 	private Long id;
 	
 	private BigDecimal subtotal;
+	
 	private BigDecimal taxaFrete;
+	
 	private BigDecimal valorTotal;
 
 	@Embedded
 	private Endereco enderecoEntrega;
 	
+	@Enumerated(EnumType.STRING)
 	private StatusPedido status;
 	
 	@CreationTimestamp
 	private OffsetDateTime dataCriacao;
 
 	private OffsetDateTime dataConfirmacao;
+	
 	private OffsetDateTime dataCancelamento;
+	
 	private OffsetDateTime dataEntrega;
 	
 	@ManyToOne
@@ -59,5 +66,21 @@ public class Pedido {
 	
 	@OneToMany(mappedBy = "pedido")
 	private List<ItemPedido> itens = new ArrayList<>();
+
+	public void calcularValorTotal() {
+	    this.subtotal = getItens().stream()
+	        .map(item -> item.getPrecoTotal())
+	        .reduce(BigDecimal.ZERO, BigDecimal::add);
+	    
+	    this.valorTotal = this.subtotal.add(this.taxaFrete);
+	}
+
+	public void definirFrete() {
+	    setTaxaFrete(getRestaurante().getTaxaFrete());
+	}
+
+	public void atribuirPedidoAosItens() {
+	    getItens().forEach(item -> item.setPedido(this));
+	}
 
 }
