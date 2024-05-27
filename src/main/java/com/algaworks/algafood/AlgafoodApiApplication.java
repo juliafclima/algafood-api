5a131b84ee1e733f9528039b9805c715144254d0
Júlia Lima <juliafclima@hotmail.com>
2024-05-27 10:31:02 -0300

    implementando serviço de envio de e-mail fake

--- src/main/java/com/algaworks/algafood/AlgafoodApiApplication.java
+++ src/main/java/com/algaworks/algafood/AlgafoodApiApplication.java
@@ -4,10 +4,13 @@ import java.util.TimeZone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+import com.algaworks.algafood.core.email.EmailProperties;
 import com.algaworks.algafood.infrastructure.repository.CustomJpaRepositoryImpl;
 
+@EnableConfigurationProperties(EmailProperties.class)
 @SpringBootApplication
 @EnableJpaRepositories(repositoryBaseClass = CustomJpaRepositoryImpl.class)
 public class AlgafoodApiApplication {
