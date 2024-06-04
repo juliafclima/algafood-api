7625db446c140b0b44761125600760a940d15a52
Júlia Lima <juliafclima@hotmail.com>
2024-06-04 15:26:31 -0300

    Corrigindo lógica de restrição de acessos para Client Credentials Flow

--- algafood/src/main/java/com/algaworks/algafood/core/security/AlgaSecurity.java
+++ algafood/src/main/java/com/algaworks/algafood/core/security/AlgaSecurity.java
@@ -29,14 +29,20 @@ public class AlgaSecurity {
 	}
 	
 	public boolean gerenciaRestaurante(Long restauranteId) {
-	    if (restauranteId == null) {
-	        return false;
-	    }
-	    
-	    return restauranteRepository.existsResponsavel(restauranteId, getUsuarioId());
-	}  
+		if (restauranteId == null) {
+			return false;
+		}
+		
+		return restauranteRepository.existsResponsavel(restauranteId, getUsuarioId());
+	}
 	
 	public boolean gerenciaRestauranteDoPedido(String codigoPedido) {
-	    return pedidoRepository.isPedidoGerenciadoPor(codigoPedido, getUsuarioId());
-	}    
+		return pedidoRepository.isPedidoGerenciadoPor(codigoPedido, getUsuarioId());
+	}
+	
+	public boolean usuarioAutenticadoIgual(Long usuarioId) {
+		return getUsuarioId() != null && usuarioId != null
+				&& getUsuarioId().equals(usuarioId);
+	}
+	
 }
