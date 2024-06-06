93227e6e985f7d52ebc0821b316aba37fd89a5d0
Júlia Lima <juliafclima@hotmail.com>
2024-06-06 16:46:30 -0300

    descrevendo documentação de endpoints de grupos

--- algafood-api-springdoc/src/main/java/com/algaworks/algafood/api/v1/model/input/GrupoInput.java
+++ algafood-api-springdoc/src/main/java/com/algaworks/algafood/api/v1/model/input/GrupoInput.java
@@ -1,15 +1,16 @@
 package com.algaworks.algafood.api.v1.model.input;
 
+import javax.validation.constraints.NotBlank;
+
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
-
-import javax.validation.constraints.NotBlank;
 
 @Setter
 @Getter
 public class GrupoInput {
 
+	@Schema(example = "Gerente")
 	@NotBlank
 	private String nome;
-	
 }
